b7b591a8ed7fbddd380b39a999e71d22b1148fb7
Alcatraz312 <77152616+Alcatraz312@users.noreply.github.com>
2023-10-04 00:36:18 +0530

    authing

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from bs4 import BeautifulSoup
 import requests
 from pprint import pprint
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+
+URL = "https://www.billboard.com/charts/hot-100/2000-08-12/"
+CLIENT_ID = "3327e5f459d44dccaf9cf5041ac1fd4d"
+CLIENT_SECRET = "1c50b1c4917046ab8e49ec56670b1e4a"
+
+auth = SpotifyOAuth(client_id= CLIENT_ID, client_secret= CLIENT_SECRET,
+                    redirect_uri= "http://example.com",)
 
 year = input("Which year do you want to travel to? Type the data in this format YYYY-MM-DD : ")
-
-billboard = requests.get("https://www.billboard.com/charts/hot-100/2000-08-12/")
+billboard = requests.get(URL)
 
 content = billboard.text
 
@@ -14,7 +23,7 @@ movies = soup.find_all(name= "h3", id = "title-of-a-story")
 
 for i in movies:
     print(i.getText())
-    
+
 
 
 
