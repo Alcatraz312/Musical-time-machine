cee5844f5d61e6223f19a967f1b0dde445dbf023
Alcatraz312 <77152616+Alcatraz312@users.noreply.github.com>
2023-10-05 23:48:31 +0530

    comments

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ URL = "https://www.billboard.com/charts/hot-100/2000-08-12/"
 CLIENT_ID = "3327e5f459d44dccaf9cf5041ac1fd4d"
 CLIENT_SECRET = "1c50b1c4917046ab8e49ec56670b1e4a"
 
+#spotify authentication
+
 auth = SpotifyOAuth(client_id= CLIENT_ID, client_secret= CLIENT_SECRET,
                     redirect_uri= "http://example.com",
                     scope= "playlist-modify-private",
@@ -18,18 +20,23 @@ auth = SpotifyOAuth(client_id= CLIENT_ID, client_secret= CLIENT_SECRET,
 
 sp = spotipy.Spotify(auth_manager= auth)
 
-# year = input("Which year do you want to travel to? Type the data in this format YYYY-MM-DD : ")
-# billboard = requests.get(URL)
-
-# content = billboard.text
-
-# soup = BeautifulSoup(content, "html.parser")
-
-# movies = soup.find_all(name= "h3", id = "title-of-a-story")
-
-# for i in movies:
-#     print(i.getText())
-
 user_id = sp.current_user()["id"]
+print(user_id)
+
+#scraping top 100 songs from the billboard
+
+year = input("Which year do you want to travel to? Type the data in this format YYYY-MM-DD : ")
+billboard = requests.get(URL)
+
+content = billboard.text
+
+soup = BeautifulSoup(content, "html.parser")
+
+movies = soup.find_all(name= "h3", id = "title-of-a-story")
+
+for i in movies:
+    print(i.getText())
+
+
 
 
