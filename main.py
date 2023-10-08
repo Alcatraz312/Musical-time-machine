016491cf11af42f909cf6267dcba8a8e9ab0b4d9
Alcatraz312 <77152616+Alcatraz312@users.noreply.github.com>
2023-10-08 17:19:57 +0530

    new commit

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from pprint import pprint
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
-
-URL = "https://www.billboard.com/charts/hot-100/2000-08-12/"
+date = input("Which year do you want to travel to? Type the data in this format YYYY-MM-DD : ")
+URL = "https://www.billboard.com/charts/hot-100/" + date
 CLIENT_ID = "3327e5f459d44dccaf9cf5041ac1fd4d"
 CLIENT_SECRET = "1c50b1c4917046ab8e49ec56670b1e4a"
 
@@ -21,11 +21,10 @@ auth = SpotifyOAuth(client_id= CLIENT_ID, client_secret= CLIENT_SECRET,
 sp = spotipy.Spotify(auth_manager= auth)
 
 user_id = sp.current_user()["id"]
-print(user_id)
 
 #scraping top 100 songs from the billboard
 
-date = input("Which year do you want to travel to? Type the data in this format YYYY-MM-DD : ")
+
 billboard = requests.get(URL)
 
 content = billboard.text
@@ -33,17 +32,37 @@ content = billboard.text
 soup = BeautifulSoup(content, "html.parser")
 
 movies = soup.find_all(name= "h3", id = "title-of-a-story")
-list_of_movies = []
+list_of_songs = []
 
 for i in movies:
     a = i.getText()
-    list_of_movies.append(a.strip())
+    list_of_songs.append(a.strip())
     
 year = date.split("-")[0]
 song_uri = []
 
-for song in list_of_movies:
+for song in list_of_songs:
     result = sp.search(q = f"track : {song} year : {year}", type= "track")
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uri.append(uri)
+
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
+
+    
+print(song_uri)
+
+
+
+
+
+
+
+
+    
+
+
 
 
 
